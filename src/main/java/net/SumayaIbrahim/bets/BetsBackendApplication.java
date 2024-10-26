c9fc20e626e091caed681c3b455f778a0b7823d4
ibraram100 <ibraram100@hotmail.com>
2024-10-26 12:02:56 +0200

    First Commit

---
+++ src/main/java/net/SumayaIbrahim/bets/BetsBackendApplication.java
@@ -0,0 +1,13 @@
+package net.SumayaIbrahim.bets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetsBackendApplication.class, args);
+	}
+
+}
