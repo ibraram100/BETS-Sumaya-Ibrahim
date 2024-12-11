513cc3e800fd3c9248428040c399175621dacfb8
Sumaya-Sharafeddin <sumaya.sharafeddin@gmail.com>
2024-12-11 21:32:45 +0200

    This should be okay for now.

---
+++ src/main/java/net/SumayaIbrahim/bets/dto/DiscountsDTO.java
@@ -0,0 +1,18 @@
+package net.SumayaIbrahim.bets.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import net.SumayaIbrahim.bets.entity.Ticket;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DiscountsDTO {
+    private long discount_id;
+    private String discount_type;
+    private float discount_amount;
+    private TicketDTO ticket;
+}
