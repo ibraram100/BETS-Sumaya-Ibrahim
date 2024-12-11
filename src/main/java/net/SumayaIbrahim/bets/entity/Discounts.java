41361c0a06a102e8d2e0f3353690956185a12ce8
Sumaya-Sharafeddin <sumaya.sharafeddin@gmail.com>
2024-12-11 21:05:41 +0200

    Joining a 'Discounts' feature,is yet to be completed.

---
+++ src/main/java/net/SumayaIbrahim/bets/entity/Discounts.java
@@ -0,0 +1,29 @@
+package net.SumayaIbrahim.bets.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
+import java.util.ArrayList;
+
+@Builder
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Discounts")
+public class Discounts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "discount_id")
+    private long discount_id;
+    @Column(name="discount_type")
+    private String discount_type;
+    @Column(name= "discount_amount")
+    private float discount_amount;
+    @ManyToOne //discounts can be applied to any ticket
+    @JoinColumn(name= "tickets")
+    private Ticket ticket;
+
+
+}
